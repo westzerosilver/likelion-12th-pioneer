479078435e0189f075ce348b9fed1c5b470e5d56
westzerosilver <oooiooop9988@gmail.com>
2024-08-01 16:32:43 +0900

    cors settings

--- backend/src/main/java/com/likelion12th/pioneer_2ne1/config/CorsMvcConfig.java
+++ backend/src/main/java/com/likelion12th/pioneer_2ne1/config/CorsMvcConfig.java
@@ -1,17 +1,25 @@
 package com.likelion12th.pioneer_2ne1.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
 
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .exposedHeaders("Set-Cookie")
+//                .exposedHeaders("Set-Cookie")
+
+                .exposedHeaders(HttpHeaders.LOCATION)
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowCredentials(true).maxAge(3600)
                 .allowedOrigins("http://localhost:3000");
     }
 
